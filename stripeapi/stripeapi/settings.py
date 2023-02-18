3828a48ffc3b14c9c715863f25eb9583b868d59b
nearbad <102875490+nearbad@users.noreply.github.com>
2023-02-18 20:29:37 +0300

    Update settings.py

--- stripeapi/stripeapi/settings.py
+++ stripeapi/stripeapi/settings.py
@@ -2,7 +2,7 @@ from pathlib import Path
 from decouple import config
 # env var
 STRIPE_SECRET_KEY = config('STRIPE_SECRET_KEY')
-STRIPE_PUBLISHABLE_KEY = config('STRIPE_PUBLIC_KEY')
+STRIPE_PUBLISHABLE_KEY = config('STRIPE_PUBLISHABLE_KEY')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
